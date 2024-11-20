57d0e50973ed2d8c24f73c8001cec9f78d89e406
Maxime Laurin <laurin.maxime1@gmail.com>
2024-11-20 15:00:37 -0500

    Remise finale

--- flask/app.py
+++ flask/app.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template, jsonify, request
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+database_url = os.getenv('DATABASE_URL')
 
 app = Flask(__name__)
+
+dbHost = process.env.DB_HOST;
+dbUser = process.env.DB_USER;
+dbPass = process.env.DB_PASSWORD;
+dbDatabase = process.env.DB_NAME;
+
 
 # Configuration de la connexion à la base de données
 def get_db_connection():
     return mysql.connector.connect(
-        host="127.0.0.7",
-        user="root",
-        passwd="mysql",
-        database="pizzaplace"
+        dbHost=DB_HOST,
+        dbUser=DB_USER,
+        dbPass=DB_PASSWORD,
+        dbDatabase=DB_NAME
     )
     
 #Afficher Index.html
@@ -17,9 +28,12 @@ def get_db_connection():
 def index():
     connection = get_db_connection()
     cursor = connection.cursor()
+
     cursor.execute("SELECT Clients.nom AS NomClient, Pizzas.sauce AS SaucePizza FROM Commandes INNER JOIN Clients ON Commandes.id_client = Clients.id INNER JOIN Pizzas ON Commandes.id_pizza = Pizzas.id;")
     data = cursor.fetchall()
+
     cursor.close()
+    connection.close()
     return render_template('index.html', data=data)
 
 #Afficher les clients
@@ -34,7 +48,6 @@ def afficher_clients():
     
     cursor.close()
     connection.close()
-    
     return jsonify(clients)
 
 #Ajouter un client
@@ -49,22 +62,25 @@ def ajouter_client():
     cursor = connection.cursor()
     cursor.execute("INSERT INTO Clients (nom, numero_telephone, adresse) VALUES (%s, %s, %s)", (nom, numero_telephone, adresse))
     connection.commit()
-    
+
+    cursor.close()
+    connection.close()
     return jsonify(success=True)
 
 #Aafficher les commandes selon un utilisateurs
 @app.route('/afficher_commande', methods=['POST'])
 def afficher_commande():
     connection = get_db_connection()
-    cursor = connection.cursor(dictionary=True)  # Renvoie un dictionnaire au lieu de tuples
+    cursor = connection.cursor(dictionary=True)
     
-    # Récupérer l'ID client du JSON envoyé par le frontend
     data = request.get_json()
     numero_client = data.get("numero_client")
 
     cursor.execute("SELECT Clients.id, Clients.nom, Pizzas.croute, Pizzas.sauce, Pizzas.garniture1, Pizzas.garniture2, Pizzas.garniture3, Pizzas.garniture4, COALESCE(Commandes_en_attente.statut, 'Terminé') statut FROM Commandes LEFT JOIN Clients ON Commandes.id_client = Clients.id LEFT JOIN Pizzas ON Commandes.id_pizza = Pizzas.id LEFT JOIN Commandes_en_attente ON Commandes.id = Commandes_en_attente.id WHERE Clients.id = %s", (numero_client,));
     commandes = cursor.fetchall()
 
+    cursor.close()
+    connection.close()
     return jsonify(commandes)
 
 #Obtenir valeurs croute pour la liste déroulante
@@ -72,12 +88,16 @@ def afficher_commande():
 def obtenirValeursCroute():
     connection = get_db_connection()
     cursor = connection.cursor()
+
     cursor.execute("SHOW COLUMNS FROM Pizzas LIKE 'croute'")
     result = cursor.fetchone()
     enum_values = result[1]
     
     enum_values = enum_values.replace("enum(", "").replace(")", "").replace("'", "")
     values_list = enum_values.split(",")
+
+    cursor.close()
+    connection.close()
     return jsonify(values_list)
 
 #Obtenir valeurs sauce pour la liste déroulante
@@ -85,12 +105,16 @@ def obtenirValeursCroute():
 def obtenirValeursSauce():
     connection = get_db_connection()
     cursor = connection.cursor()
+
     cursor.execute("SHOW COLUMNS FROM Pizzas LIKE 'sauce'")
     result = cursor.fetchone()
     enum_values = result[1]
     
     enum_values = enum_values.replace("enum(", "").replace(")", "").replace("'", "")
     values_list = enum_values.split(",")
+
+    cursor.close()
+    connection.close()
     return jsonify(values_list)
 
 #Obtenir valeurs garniture pour la liste déroulante
@@ -98,20 +122,23 @@ def obtenirValeursSauce():
 def obtenirValeursGarniture():
     connection = get_db_connection()
     cursor = connection.cursor()
+
     cursor.execute("SHOW COLUMNS FROM Pizzas LIKE 'garniture1'")
     result = cursor.fetchone()
     enum_values = result[1]
     
     enum_values = enum_values.replace("enum(", "").replace(")", "").replace("'", "")
     values_list = enum_values.split(",")
+
+    cursor.close()
+    connection.close()
     return jsonify(values_list)
 
 #Afficher les commandes en attentes
 @app.route('/afficher_attente', methods=['POST'])
 def afficher_attente():
     connection = get_db_connection()
-    cursor = connection.cursor(dictionary=True)  # Renvoie un dictionnaire au lieu de tuples
-    # Récupérer l'ID client du JSON envoyé par le frontend
+    cursor = connection.cursor(dictionary=True)
     data = request.get_json()
     id = data.get("numero_client")
     
@@ -133,16 +160,15 @@ def ajouter_pizza():
     garniture3 = data.get("garniture3")
     garniture4 = data.get("garniture4")
 
-    # Connexion à la base de données
     connection = get_db_connection()
     cursor = connection.cursor()
 
-    # Requête SQL d'insertion
     cursor.execute("INSERT INTO Pizzas (croute, sauce, garniture1, garniture2, garniture3, garniture4) VALUES (%s, %s, %s, %s, %s, %s)", (croute, sauce, garniture1, garniture2, garniture3, garniture4))
     connection.commit()
 
+    cursor.close()
+    connection.close()
     return jsonify(success=True)
-
 
 
 if __name__ == '__main__':
